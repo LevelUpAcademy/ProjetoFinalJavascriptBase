c687f3a63a6c40045e4361c4fc5b796a04f84963
Jonatan Kapps <jkapps@MacBook-Air-de-Jonatan.local>
2017-09-02 14:01:38 -0300

    New controller

--- js/controllers/carscontroller.js
+++ js/controllers/carscontroller.js
@@ -3,19 +3,33 @@ var app = angular.module('garage')
 app.controller('CarsController', function($scope, $http){
   $scope.cars = []
   $scope.message = "Carregando carros..."
-  $http({
-    method: 'GET',
-    url: 'https://aulasjavascript.herokuapp.com/cars'
-  })
-  .then(function(cars){
-    $scope.cars = cars.data
-    $scope.message = ""
-  },
-  function(err){
-    alert('Ocorreu um erro!')
-    console.log(err)
-  })
 
+  function Load(){
+    $http({
+      method: 'GET',
+      url: 'https://aulasjavascript.herokuapp.com/cars'
+    })
+    .then(function(cars){
+      $scope.cars = cars.data
+      $scope.message = ""
+    },
+    function(err){
+      alert('Ocorreu um erro!')
+      console.log(err)
+    })
+  }
+
+  Load()
+
+  $scope.Delete = function(id){
+    $http.delete('https://aulasjavascript.herokuapp.com/cars/' + id)
+    .then(function(data){
+      Load()
+    },
+    function(err){
+      console.log(err)
+    })
+  }
 })
 
 app.controller('CarsNewController', function($scope, $http){
--- js/controllers/garagecontroller.js
+++ js/controllers/garagecontroller.js
@@ -1,5 +1,12 @@
 var app = angular.module('garage')
 
 app.controller('GarageController', function($scope){
-
+  $scope.cars = []
+  $http.get('https://aulasjavascript.herokuapp.com/cars')
+  .then(function(cars){
+    $scope.cars = cars.data
+    },
+  function(err){
+    console.log(err)
+  })
 });
