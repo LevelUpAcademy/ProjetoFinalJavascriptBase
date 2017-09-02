f9ccac7db2a2cb13b970d38e5f2cb4c78651582d
Jonatan Kapps <jkapps@MacBook-Air-de-Jonatan.local>
2017-09-02 15:14:32 -0300

    Calculate value

--- js/controllers/garagecontroller.js
+++ js/controllers/garagecontroller.js
@@ -3,6 +3,7 @@ var app = angular.module('garage')
 app.controller('GarageController', function($scope, $http){
   $scope.car = {}
   $scope.cars = []
+  $scope.parkedCars = []
   $scope.visible = false
 
   $http.get('https://aulasjavascript.herokuapp.com/cars')
@@ -22,5 +23,34 @@ app.controller('GarageController', function($scope, $http){
     function(err){
       console.log(err)
     })
+  }
+
+  $scope.Park = function(){
+    var carToPark = {
+      car: $scope.car,
+      in: new Date(),
+      value: 0.0,
+      hourValue: $scope.valueHour
+    }
+
+
+    $scope.parkedCars.push(carToPark)
+    $scope.visible = false
+  }
+
+  $scope.CalcValue = function(){
+    for (var i = 0; i < $scope.parkedCars.length; i++) {
+      var hourValue = $scope.parkedCars[i].hourValue
+      var inHour = $scope.parkedCars[i].in
+
+      var totalValue = ((hourValue / 60) * millisToMinutesAndSeconds(inHour - new Date())) * -1
+      console.log("valor" + totalValue)
+      $scope.parkedCars[i].value = totalValue
+    }
+  }
+
+  function millisToMinutesAndSeconds(millis) {
+    var minutes = Math.floor(millis / 60000);
+    return minutes
   }
 })
